10c897d235dc2d154fb6aed66f7861f02734752f
Gilbert Pellegrom <gilbert@pellegrom.me>
2017-01-02 15:52:51 +0000

    Update web pack config

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ if (process.env.NODE_ENV === 'production') {
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
+      sourceMap: false,
       compress: {
         warnings: false
       }
